5e3fdc7c68237a3e98fa6dc449fe969c450f8cc1
jhrfeng <jhrzhongyue@gmail.com>
2017-05-26 23:29:08 +0800

    优化

--- src/pages/chart/chart.ts
+++ src/pages/chart/chart.ts
@@ -13,6 +13,7 @@ export class ChartPage {
 
 	  screenWidth:any = window.screen.availWidth;
  	  screenHeight:any = window.screen.availHeight;
+    charUrl:string = "";
     item:any = {type:'', token:'', expertid:'', keyword:''};
 
   	constructor(public navCtrl: NavController, 
@@ -26,41 +27,42 @@ export class ChartPage {
         this.item.type = navParams.get('type');
         this.item.expertid = navParams.get('id');
         this.item.keyword = navParams.get('keyword');
+        this.chartUrl();
       })
     }
 
   	ionViewDidLoad() {
+      // this.auth.loadings();
     	console.log('ionViewDidLoad ChartPage');
   	}
 
   	chartUrl() {
         // this.auth.loadings();
         if(this.item.type == 'socialNetwork'){
-          let url =  REQUEST_URL + '/tpl/html5/socialNetwork.html?expertId=' + this.item.expertid + '&token=' + this.item.token;
-          console.log(url);
-          return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+          this.charUrl =  REQUEST_URL + '/tpl/html5/socialNetwork.html?expertId=' + this.item.expertid + '&token=' + this.item.token;
+          //let url console.log(url);
+          //return this.sanitizer.bypassSecurityTrustResourceUrl(charUrl);
         }
         else if(this.item.type == 'radarGraphic'){
-          let url =  REQUEST_URL + '/tpl/html5/radarGraphic.html?expertId=' + this.item.expertid + '&token=' + this.item.token;
-          // let url =  REQUEST_URL + '/#/radarGraphic/' + this.item.token + '/' + this.item.expertid;
-          console.log(url);
-          return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+          this.charUrl =  REQUEST_URL + '/tpl/html5/radarGraphic.html?expertId=' + this.item.expertid + '&token=' + this.item.token;
+          //let url console.log(url);
+          //return this.sanitizer.bypassSecurityTrustResourceUrl(url);
         }
         else if(this.item.type == 'relevantInstitutions'){
           // this.auth.loadings();
-          let url =  REQUEST_URL + '/tpl/html5/relevantInstitutions.html?token=' + this.item.token + '&keyword=' + this.item.keyword +'&id=relevantInstitutions';
-          console.log(url);
-          return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+          this.charUrl =  REQUEST_URL + '/tpl/html5/relevantInstitutions.html?token=' + this.item.token + '&keyword=' + this.item.keyword +'&id=relevantInstitutions';
+          //let url console.log(url);
+          //return this.sanitizer.bypassSecurityTrustResourceUrl(url);
         }
         else if(this.item.type == 'relevantExperts'){
-          let url =  REQUEST_URL + '/tpl/html5/relevantExperts.html?token=' + this.item.token + '&keyword=' + this.item.keyword + '&id=relevantExperts';
-          console.log(url);
-          return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+          this.charUrl =  REQUEST_URL + '/tpl/html5/relevantExperts.html?token=' + this.item.token + '&keyword=' + this.item.keyword + '&id=relevantExperts';
+          //let url console.log(url);
+          //return this.sanitizer.bypassSecurityTrustResourceUrl(url);
         }
         else if(this.item.type == 'resarchTrends'){
-          let url =  REQUEST_URL + '/tpl/html5/researchTrends.html?token=' + this.item.token + '&keyword=' + this.item.keyword + '&id=researchTrends';
-          console.log(url);
-          return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+          this.charUrl =  REQUEST_URL + '/tpl/html5/researchTrends.html?token=' + this.item.token + '&keyword=' + this.item.keyword + '&id=researchTrends';
+          //let url console.log(url);
+          //return this.sanitizer.bypassSecurityTrustResourceUrl(url);
         }
 	  }
 
